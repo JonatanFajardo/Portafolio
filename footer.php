b33a437bb6b4682af7d4a972ae62f1edbcc7fa6f
JonatanFajardo <69583436+JonatanFajardo@users.noreply.github.com>
2022-03-24 16:44:13 -0600

    first commit

---
+++ footer.php
@@ -0,0 +1,43 @@
+<div class="advertencia">
+      <div class="container-full">
+      <div class="row">
+            <!-- <div class="col-1 ">
+                <div class="vl"></div>
+                </div> -->
+                <div class="col-11 text">
+            
+        <p><span>Advertencia</span> esta pagina esta en desarrollo</p></div>
+        </div>
+      </div>
+    </div>
+
+
+<div class='container-full' id="contact">
+    <div class="container">
+      <div class='row'>
+        <div class='col-8'>
+          <h2>¿Preparado para comenzar?</h2>
+        </div>
+        <div class="col-4">
+          <a href="https://api.whatsapp.com/send?phone=50497523424&text=Hola,%20soy%20Jonatan%20Fajardo.%20En%20que%20le%20puedo%20servir?"
+            target="_blank" class="btn button-wrapper">Contactame</a>
+        </div>
+      </div>
+    </div>
+  </div>  
+  
+  
+  <!--  Jquery js file  -->
+  <script src="assets/js/jquery-min.js"></script>
+
+  <!--  isotope js library  -->
+  <script src="assets/js/isotope/isotope.min.js"></script>
+
+  <!--  custom js file  -->
+  <script src="assets/js/Main.js"></script>
+
+  <!-- font awesonme -->
+  <script src="https://kit.fontawesome.com/1f52c15500.js" crossorigin="anonymous"></script>
+
+  <!--  Magnific popup script file  -->
+  <script src="assets/js/jquery.magnific-popup.min.js"></script>
